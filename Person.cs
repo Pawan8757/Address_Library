f7abd59f58170804b5ebb4495e614787dfdf8732
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-03-16 22:30:55 +0530

    adding initializing file

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookLamdaDemo
+{
+    public class Person
+    {
+        public string PhoneNo;
+        public string Name;
+        public string Address;
+
+        public int Age;
+
+        public Person(string phoneno, string name, string address, int age)
+        {
+            PhoneNo = phoneno;
+            Name = name;
+            Address = address;
+            Age = age;
+        }
+    }
+}
